9618987bdb4f8a182ecc7b5eb290ed31a948f7b0
Omnipresent <omnipresent@Omnipresents-Mac-mini-2.local>
2014-02-18 13:13:22 +0530

    Initial Commit

---
+++ ImagePicker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ImagePicker
+//
+//  Created by Omnipresent on 18/02/14.
+//  Copyright (c) 2014 iOSCuriosity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ImagePicker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ImagePicker
+//
+//  Created by Omnipresent on 18/02/14.
+//  Copyright (c) 2014 iOSCuriosity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
